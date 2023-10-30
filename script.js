de551c892789c54036c4c2a3420e7c2938ddefb9
kuruwata <109745695+kuruwata@users.noreply.github.com>
2023-10-30 15:12:46 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// JavaScript
+
+//実行する機能
+//const show = (entries) => {
+//    //console.log(entries[0].target);
+//    entries.forEach((entry) => {
+//        const keyframes = {
+//            opacity: [0, 1],
+//           translate: ['200px 0', 0]
+//     }
+//      entries[0].target.animate(keyframes, 600);
+//  });
+//}
+
+//一度表示したら動作を止める
+const show = (entries, obs) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            const keyframes = {
+                opacity: [0, 1],
+                translate: ['-200px 0', 0]
+            }
+            entry.target.animate(keyframes, 600);
+
+            //一度表示されたら止める
+            obs.unobserve(entry.target);
+        }
+    });
+}
+
+//監視機能の設置
+const observer = new IntersectionObserver(show);
+
+//監視対象の指示
+const images = document.querySelectorAll('.img');
+images.forEach(img => {
+    observer.observe(img);
+});
